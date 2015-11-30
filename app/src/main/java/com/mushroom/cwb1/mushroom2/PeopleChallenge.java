362c7a8b44f092ea21a3cdd36a51d571a6a2f433
0maxime <pittomvils-maxime@hotmail.com>
2015-11-30 23:49:10 +0100

    ranking

---
+++ app/src/main/java/com/mushroom/cwb1/mushroom2/PeopleChallenge.java
@@ -0,0 +1,125 @@
+package com.mushroom.cwb1.mushroom2;
+
+
+public class PeopleChallenge {
+    private String user1;
+    private String user2;
+    private int user1_status;
+    private int user2_status;
+    private String challenge_name;
+    private String user1_result;
+    private String user2_result;
+    private long start;
+    private long end;
+    private long user1_ended;
+    private long user2_ended;
+    private String winner;
+
+
+
+    public PeopleChallenge(){
+
+    }
+
+
+
+    // GETTERS AND SETTERS
+
+    public String getUser1() {
+        return user1;
+    }
+
+    public void setUser1(String user1) {
+        this.user1 = user1;
+    }
+
+    public String getUser2() {
+        return user2;
+    }
+
+    public void setUser2(String user2) {
+        this.user2 = user2;
+    }
+
+    public int getUser1_status() {
+        return user1_status;
+    }
+
+    public void setUser1_status(int user1_status) {
+        this.user1_status = user1_status;
+    }
+
+    public int getUser2_status() {
+        return user2_status;
+    }
+
+    public void setUser2_status(int user2_status) {
+        this.user2_status = user2_status;
+    }
+
+    public String getChallenge_name() {
+        return challenge_name;
+    }
+
+    public void setChallenge_name(String challenge_name) {
+        this.challenge_name = challenge_name;
+    }
+
+    public String getUser1_result() {
+        return user1_result;
+    }
+
+    public void setUser1_result(String user1_result) {
+        this.user1_result = user1_result;
+    }
+
+    public String getUser2_result() {
+        return user2_result;
+    }
+
+    public void setUser2_result(String user2_result) {
+        this.user2_result = user2_result;
+    }
+
+    public long getStart() {
+        return start;
+    }
+
+    public void setStart(long start) {
+        this.start = start;
+    }
+
+    public long getEnd() {
+        return end;
+    }
+
+    public void setEnd(long end) {
+        this.end = end;
+    }
+
+    public long getUser1_ended() {
+        return user1_ended;
+    }
+
+    public void setUser1_ended(long user1_ended) {
+        this.user1_ended = user1_ended;
+    }
+
+    public long getUser2_ended() {
+        return user2_ended;
+    }
+
+    public void setUser2_ended(long user2_ended) {
+        this.user2_ended = user2_ended;
+    }
+
+    public String getWinner() {
+        return winner;
+    }
+
+    public void setWinner(String winner) {
+        this.winner = winner;
+    }
+}
+
+
